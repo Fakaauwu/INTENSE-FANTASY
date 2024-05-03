64ca091ac8c00de685d55fd7118008b5d2b948cb
Fakaauwu <129827287+Fakaauwu@users.noreply.github.com>
2024-05-03 07:49:10 -0600

    Add files via upload

---
+++ INTENSE FANTASY 2.0/ppi7am-main/src/main/java/com/udg/siiau/controllers/WebConfig.java
@@ -0,0 +1,22 @@
+package com.udg.siiau.controllers;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342") // Cambia por tu origen permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
